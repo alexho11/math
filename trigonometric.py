2543979c389e03e52ddb7e663b190479c4c6fb10
Alex Ho <alex@Alexs-MacBook-Pro.local>
2022-06-23 19:15:13 +0200

    new

--- trigonometric.py
+++ trigonometric.py
@@ -3,10 +3,21 @@ import numpy as np
 import sympy as sp
 from linear import lin
 from tri import tri
+
 p=[(0.3,0.4),(0.4,0.2),(0.5,0.6),(0.2,0.7)]#points
+
+
+while(1):
+    pn=input("coordinate(x,y) (0=exit): ").split()
+    pn=[float(i) for i in pn]
+    if pn == [0]:
+        break
+    pn=p.append(pn)
+
 n=len(p)-1
 pix,piy=tri(p)
 x,y=lin(p)
+
 plt.plot(x,y,label='linear interpolat')
 plt.plot(pix,piy,label='trigonometric interpolat')
 plt.scatter(x,y,label='interpolationspunkte')
