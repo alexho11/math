f2583c069a34d56fb38aaf1b01c5dc5614760c01
Alex Ho <alex@Alexs-MacBook-Pro.local>
2022-06-24 20:38:52 +0200

    interactive add point

--- trigonometric.py
+++ trigonometric.py
@@ -8,11 +8,14 @@ p=[(0.3,0.4),(0.4,0.2),(0.5,0.6),(0.2,0.7)]#points
 
 
 while(1):
-    pn=input("coordinate(x,y) (0=exit): ").split()
+    pn=input("coordinate(x,y) (0=exit): ").split(',')
     pn=[float(i) for i in pn]
     if pn == [0]:
         break
-    pn=p.append(pn)
+    elif np.isreal(pn).all()==True and len(pn)==2:
+        pn=p.append(pn)
+    else:
+        print("Invalid")
 
 n=len(p)-1
 pix,piy=tri(p)
@@ -24,4 +27,27 @@ plt.scatter(x,y,label='interpolationspunkte')
 for i in range(n+1):
     plt.annotate('p'+str(i),p[i],(p[i][0]+0.01,p[i][1]+0.01))#annotate
 plt.legend()
+plt.show(block=False)
+
+while(1):
+    pn=input("coordinate(x,y) (0=exit): ").split(',')
+    pn=[float(i) for i in pn]
+    if pn == [0]:
+        break
+    elif np.isreal(pn).all()==True and len(pn)==2:
+        pn=p.append(pn)
+    else:
+        print("Invalid")
+
+n=len(p)-1
+pix,piy=tri(p)
+x,y=lin(p)
+plt.clf()
+plt.plot(x,y,label='linear interpolat')
+plt.plot(pix,piy,label='trigonometric interpolat')
+plt.scatter(x,y,label='interpolationspunkte')
+for i in range(n+1):
+    plt.annotate('p'+str(i),p[i],(p[i][0]+0.01,p[i][1]+0.01))#annotate
+plt.legend()
+plt.draw()
 plt.show()
